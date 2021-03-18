8ace49294c9af0b42fb9d84d313ef50ada0dc2f9
Felipe Barros <felipe.b4rros@gmail.com>
2021-03-18 11:07:21 -0300

    overwrite method save adding lat/lon fields

--- mapProj/core/models.py
+++ mapProj/core/models.py
@@ -7,7 +7,9 @@ class FloraOccurrence(models.Model):
     species_name = models.CharField(max_length=100, verbose_name='Nomre de la especie', null=False)
     Observations = models.TextField(verbose_name='Observaciones')
     picture = models.ImageField()
-    geom = PointField()
+    lon = models.FloatField()
+    lat = models.FloatField()
+    geom = PointField(null=True, blank=True)
 
     def __str__(self):
         return self.species_name
@@ -15,3 +17,10 @@ class FloraOccurrence(models.Model):
     @property
     def picture_url(self):
         return self.picture.url
+
+    def save(self, *args, **kwargs):
+        if self.lon is None or self.lat is None:
+            self.geom = {'type': 'Point', 'coordinates': [0, 0]}
+        else:
+            self.geom = {'type': 'Point', 'coordinates': [self.lon, self.lat]}
+        super(FloraOccurrence, self).save(*args, **kwargs)
